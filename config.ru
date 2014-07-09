efa9852dd35d644bb43a4644978b050f4e389156
meetme2meat <meetme2meat@gmail.com>
2014-07-09 15:56:48 +0530

    Fix the UI

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './speed'
+run Sinatra::Application
--- speed.rb
+++ speed.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'sinatra'
 %w(sample128k sample256k sample512k sample1024k sample2048k sample4096k sample8192k).each do |url|
   get "/#{url}" do
@@ -8,4 +7,8 @@ end
 
 get '/' do
   erb :index
+end
+
+get '/favicon.ico' do
+  sendfile File.expand_path('public/favicon.ico')
 end
