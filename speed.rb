bf0df204653983a5d0f13df3caae43c8d5afcfec
meetme2meat <meetme2meat@gmail.com>
2014-07-18 17:32:16 +0530

    Allow Access Control to all

--- speed.rb
+++ speed.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+before do 
+  response['Access-Control-Allow-Origin'] = '*'
+end
 %w(unified sample128k_2 sample128k sample256k sample512k sample1024k sample2048k sample4096k sample8192k).each do |url|
   get "/#{url}" do
     erb url.to_sym
