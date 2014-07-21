e8a48f5be2069e36fbf5dd1ab2046a965af96419
meetme2meat <meetme2meat@gmail.com>
2014-07-21 19:02:24 +0530

    Added Speed .erb

--- speed.rb
+++ speed.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-before do 
+before do
   response['Access-Control-Allow-Origin'] = '*'
 end
 %w(unified sample128k_2 sample128k sample256k sample512k sample1024k sample2048k sample4096k sample8192k).each do |url|
@@ -10,6 +10,10 @@ end
 
 get '/test.png' do
   ''
+end
+
+get '/speed' do
+  erb :speed
 end
 
 get '/' do
